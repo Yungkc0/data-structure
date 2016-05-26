786421fe8f934f1329f7e073832e11986e18f5a6
yungkc0 <yangren15@lzu.edu.cn>
2016-05-26 09:45:48 +0800

    string updated

--- string.c
+++ string.c
@@ -13,7 +13,7 @@ char *strncpy(char *dest, const char *src, size_t n)
 {
 	char *tmp = dest;
 
-	for ( ; n >= 0; --n) {
+	for ( ; n > 0; --n) {
 		if ((*tmp = *src) != 0)
 			src++;
 		tmp++;
@@ -41,8 +41,10 @@ int strcmp(const char *s1, const char *s2)
 int strncmp(const char *s1, const char *s2, size_t n)
 {
 	for ( ; n > 0; --n)
-		if (*s1++ != *s2++)
+		if (*s1 != *s2)
 			return *s1 - *s2;
+		else
+			s1++, s2++;
 	return 0;
 }
 
@@ -83,8 +85,7 @@ char *strdel(char *s, size_t pos, size_t len)
 	char *s1, *s2;
 
 	l = strlen(s);
-	if (pos >= 0 && pos <= l &&
-			len >= 0 && len <= l - pos + 1) {
+	if (pos <= l &&	len <= l - pos + 1) {
 		s1 = s + pos;
 		s2 = s + pos + len;
 		while ((*s1++ = *s2++) != 0)
@@ -95,11 +96,46 @@ char *strdel(char *s, size_t pos, size_t len)
 	}
 }
 
+char *strins(char *s, size_t pos, const char *t)
+{
+	size_t l1, l2, i;
+	char *s1, *s2;
+
+	l1 = strlen(s);
+	if (pos <= l1) {
+		l2 = strlen(t);
+		s1 = s + l1 + l2;
+		s2 = s + l1;
+		for (i = l1 - pos + 1; i > 0; --i)
+			*s1-- = *s2--;
+		strncpy(s + pos, t, l2);
+		return s;
+	} else {
+		return NULL;
+	}
+}
+
+char *strrpl(char *s, const char *t, const char *v)
+{
+	size_t len;
+	char *p;
+
+	len = strlen(t);
+	if (!len)
+		return s;
+	int i;
+	for ( i = 3;i > 0 && (p = strstr(s, t)) != NULL;--i) {
+		strdel(s, p - s, len);
+		strins(s, p - s, v);
+	}
+	return s;
+}
+
 int main(void)
 {
 	char c[20] = "abcdef";
 
-	printf("%s\n", strdel(c, 0, 8));
-
+	printf("%s\n", c);
+	printf("%s\n", strrpl(c, "c", "**"));
 	return 0;
 }
